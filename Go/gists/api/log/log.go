81b5cd0af2077307e90a4dcf1b2f9e105d9911a5
F.LuVx <yeyu0926@hotmail.com>
2021-05-08 23:42:45 +0800

    update

--- Go/gists/api/log/log.go
+++ Go/gists/api/log/log.go
@@ -2,6 +2,7 @@ package log
 
 import (
     log "github.com/sirupsen/logrus"
+    l "log"
 )
 
 func a() {
@@ -10,4 +11,6 @@ func a() {
     }).Info("foo bar")
 
     log.WithField("json", "json1").Infoln("foo bar")
+
+    l.Println("foo...bar...")
 }
